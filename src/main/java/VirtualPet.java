eb80f09284dd5aca2e71bac040a974ecbaaa643e
Jonathan Ohueri <jonathan.ohueri@gmail.com>
2017-10-14 13:11:29 -0400

    Created a main class and model. Still working on that app.

--- src/main/java/VirtualPet.java
+++ src/main/java/VirtualPet.java
@@ -1,4 +1,88 @@
 
 public class VirtualPet {
+	// Instance Data
+	private int hunger; // instance variable
+	private int boredom;
+	private int affection;
+	private int bathroom;
+	private int thirst;
+	private String name;
+	private String description;
 
+	// Constructor
+	public VirtualPet(int hunger, int boredom, int affection, int bathroom, int thirst) {
+		this.hunger = hunger;
+		this.boredom = boredom;
+		this.affection = affection;
+		this.bathroom = bathroom;
+		this.thirst = thirst;
+	}
+
+	// Overloading Constructor
+	public VirtualPet(String name, String description) {
+		this.name = name;
+		this.description = description;
+	}
+
+	// Tick Method
+	 public void tick() { 
+		int time = 1;
+	 hunger += time;
+	 boredom += time;
+	 affection += time;
+	 bathroom += time;
+	 thirst += time;
+	 }
+
+	// Gettor
+	public int getHunger() {
+		return hunger;
+	}
+
+	public int getBordeom() {
+		return boredom;
+	}
+
+	public int getAffection() {
+		return boredom;
+	}
+
+	public int getBathroom() {
+		return boredom;
+	}
+
+	public int getThirst() {
+		return boredom;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getDescription() {
+		return name;
+	}
+
+	// Method of Behavior
+	public void feedPet(int food) {
+		hunger -= food;
+		thirst += (food / 2);
+	}
+
+	public void playPet(int play) {
+		boredom -= play;
+	}
+
+	public void rubPet(int rub) {
+		affection -= rub;
+	}
+
+	public void letOut(int poop) {
+		bathroom -= poop;
+	}
+
+	public void giveWater(int water) {
+		thirst -= water;
+		bathroom += (water / 3);
+	}
 }
---
+++ src/main/java/VirtualPetShelter.java
@@ -0,0 +1,31 @@
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class VirtualPetShelter {
+
+	// map needs 2 type params (1 for key and 1 for the value)
+		Map<String, VirtualPet> pets = new HashMap<String, VirtualPet>();
+		
+		public Collection<VirtualPet> petStats() {
+			return pets.values();
+}
+		
+		public void add(VirtualPet pet) {
+			pets.put(pet.getName(), pet); // adds a pet
+		}
+		
+		public VirtualPet getPet(String petName) {
+			return pets.get(petName); // this is a getter that pulls one individual pet name, which will serve as the key to your map. 
+		}
+		
+		public VirtualPet adoptPet(String petName) {
+			return pets.remove(petName); // whatever account has the key that I reference, get rid of it. 
+		}
+		// Pet Tick Method
+		public void  petsTick() {  //// WORK ON that syntax
+		for (VirtualPet currentPet : pets.values()) {
+			currentPet.tick(); // In the app, you will need .get(tick). I think. Ugh. 
+}
+		}
+}
